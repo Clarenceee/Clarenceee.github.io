035c2cf9790d244672a9a81a5ca975d1b641b58e
tehjayson1004 <58066874+tehjayson1004@users.noreply.github.com>
2021-04-20 21:45:29 +0800

    Add files via upload

---
+++ display.js
@@ -0,0 +1,112 @@
+var mqtt;
+var reconnectTimeout = 2000;
+var host="outstanding-engineer.cloudmqtt.com";
+var port=443;
+//var acl = new Accelerometer({frequency:60});
+var location_array = [0,0];
+var mqtt_user_data = "user_data"
+var user_data = 0;
+// Create a client instance
+var client = new Paho.MQTT.Client(host, port,"client_id");
+var found = 0;
+var user_list = 0;
+var queryString = decodeURIComponent(window.location.search);
+queryString = queryString.substring(1);
+var queries = queryString.split("=");
+var username = queries[1];
+console.log(username);
+//Example client = new Paho.MQTT.Client("m11.cloudmqtt.com", 32903, "web_" + parseInt(Math.random() * 100, 10));
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+var options = {
+useSSL: true,
+userName: "odzguzja",
+password: "XIC7binxmQ8e",
+onSuccess:onConnect,
+onFailure:doFail
+}
+
+// connect the client
+client.connect(options);
+function dataProcessing(user_data)
+{
+	var mqtt_name = user_data.driver_user_name;
+	var latitude = user_data.latitude;
+	var longitude = user_data.longitude;
+	var eta = user_data.eta;
+	var line = user_data.line;
+	var dest_lat = user_data.dest_lat;
+	var dest_long = user_data.dest_long;
+	var line_obj = [];
+	if (mqtt_name == username)
+	{
+		const image = "https://img.icons8.com/android/24/000000/car.png";
+		const dest_image = "https://img.icons8.com/android/24/000000/home.png";
+		var mapProp= {
+			  center:new google.maps.LatLng(latitude,longitude),
+			  zoom:15,
+			};
+		
+		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+		var markerOptions = {
+			position: new google.maps.LatLng(latitude,longitude),
+			icon: image,
+			map: map
+		}
+		var myMarker = new google.maps.Marker(markerOptions);
+		var dest_markerOptions = {
+			position: new google.maps.LatLng(dest_lat,dest_long),
+			icon: dest_image,
+			map: map
+		}
+		line.forEach(function (val) {
+			line_obj.push({lat: val[0], lng: val[1]});
+		});
+			
+		var dest_marker = new google.maps.Marker(dest_markerOptions);
+		var path = new google.maps.Polyline({
+			path:line_obj,
+			strokeColor: '#FF0000',
+			strokeWeight: 2
+		});
+		path.setMap(map);
+		document.getElementById("eta").innerHTML = "Your parcel will arrive at: " + eta;
+		// set up connection between html and node red
+	}
+}
+// called when the client connects
+function onConnect() {
+// Once a connection has been made, make a subscription and send a message.
+	console.log("onConnect");
+	client.subscribe(mqtt_user_data);
+	requestData(username);
+}
+
+function doFail(e){
+console.log(e);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+if (responseObject.errorCode !== 0) {
+  console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+function requestData (username)
+{
+	var user_correct_name = new Paho.MQTT.Message(username);
+	user_correct_name.destinationName = "user_correct_name";
+	client.send(user_correct_name);
+}
+// called when a message arrives
+function onMessageArrived(message) {
+	if (message.destinationName == mqtt_user_data)
+	{
+		//user_data = message.payloadString.split(",");
+		user_data = JSON.parse(message.payloadString);
+		console.log(user_data);
+		dataProcessing(user_data);
+	}	
+	console.log("Something's here");
+}
---
+++ main.js
@@ -0,0 +1,85 @@
+var mqtt;
+var reconnectTimeout = 2000;
+var host="outstanding-engineer.cloudmqtt.com";
+var port=443;
+var now = new Date();
+var acc_start = now.getTime();
+var dt_orient = 0;
+//var acl = new Accelerometer({frequency:60});
+var driver_id = -1;
+var corr  = 0;
+console.log(window.navigator);
+var location_array = [0,0];
+var mqtt_username = "allusernames";
+var mqtt_user_data = "user_data"
+var user_data = 0;
+// Create a client instance
+var client = new Paho.MQTT.Client(host, port,"client_id");
+var found = 0;
+var user_list = 0;
+//Example client = new Paho.MQTT.Client("m11.cloudmqtt.com", 32903, "web_" + parseInt(Math.random() * 100, 10));
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+var options = {
+useSSL: true,
+userName: "odzguzja",
+password: "XIC7binxmQ8e",
+onSuccess:onConnect,
+onFailure:doFail
+}
+///////need to identify driver, make connection with mqtt
+// connect the client
+client.connect(options);
+
+// called when the client connects
+function onConnect() {
+// Once a connection has been made, make a subscription and send a message.
+	console.log("onConnect");
+	client.subscribe(mqtt_username);
+}
+
+function doFail(e){
+console.log(e);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+if (responseObject.errorCode !== 0) {
+  console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+function compareName (name)
+{
+	user_enter_name = document.getElementById("name").value;
+	if (user_enter_name == name)
+	{
+		found = 1;
+	}
+}
+// called when a message arrives
+function onMessageArrived(message) {
+	if (message.destinationName == mqtt_username)
+	{
+		user_list = message.payloadString.split(",");
+		//console.log("onMessageArrived:"+ found);
+	}
+	console.log("Something's here");
+}
+function checkUser()
+{
+	found = 0;
+	if (user_list != 0)
+	{
+		user_list.forEach(compareName);
+	}
+	if (found == 0)
+	{
+		alert("Your parcel is not delivered today. Please make sure user's name entered is correct :)");
+	}
+	else
+	{
+		window.location.href = "display.html?username=" + user_enter_name;
+	}
+	console.log("found " + found);
+}
